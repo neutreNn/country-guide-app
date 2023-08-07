d6772abdd4fcb726b344d6b5931a0cf1b613c271
neutreNn <136928661+neutreNn@users.noreply.github.com>
2023-08-07 20:08:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function numberWithSpaces(word) {
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
     let finalUrl = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-    console.log(finalUrl);
     fetch(finalUrl).then((response) => response.json())
     .then((data) => {
         spacesPopulation = numberWithSpaces(data[0].population);
